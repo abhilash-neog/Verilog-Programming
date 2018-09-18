e2ab8aa61e90c3f22588cb5208d4cbf25598ceb3
Abhilash <abhineog97@gmail.com>
2018-09-19 01:07:10 +0530

    new file

--- Lab2/fadder_behav.v
+++ Lab2/fadder_behav.v
@@ -3,11 +3,21 @@ input [3:0] a, b;
 input cin;
 output [3:0] s;
 output cout;
+reg temp1,temp2;
+integer i;
 
 always@(a or b)
-begin
-{cout,s} = a+b+cin;
-end
 
+for(i=0;i<=3;i=i+1)
+begin
+temp1 = a[i] ^ b[i];
+s[i] = temp1 ^ cin;
+
+temp2 = a[i]&b[i];
+cin = temp2||(temp1 & cin);
+
+if(i==3) cout = cin;
+
+end
 endmodule
 
