88be7a441b9323f458eadac543ab16832888efa8
Abhilash <abhineog97@gmail.com>
2018-09-19 01:55:46 +0530

    final change

--- Lab2/fadder_behav.v
+++ Lab2/fadder_behav.v
@@ -1,23 +1,26 @@
 module fadder_behav(a,b,cin,s,cout);
 input [3:0] a, b;
 input cin;
-output [3:0] s;
-output cout;
+output reg temp;
+output reg [3:0] s;
+output reg cout;
 reg temp1,temp2;
 integer i;
-
+initial
+temp = cin;
 always@(a or b)
 
 for(i=0;i<=3;i=i+1)
 begin
 temp1 = a[i] ^ b[i];
 s[i] = temp1 ^ cin;
-
 temp2 = a[i]&b[i];
-cin = temp2||(temp1 & cin);
+temp = temp2||(temp1 & temp);
 
-if(i==3) cout = cin;
-
+if(i==3) 
+begin
+cout = cin;
+end
 end
 endmodule
 
