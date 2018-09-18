fd8f49295e018e8a60119ff9f0ce7e8cd0b3ee39
Abhilash <abhineog97@gmail.com>
2018-09-19 00:02:35 +0530

    changes

--- Lab1/4bitMagnitudeComp_behav.v
+++ Lab1/4bitMagnitudeComp_behav.v
@@ -15,3 +15,31 @@ else
 altb=1;
 end
 end module
+
+module testbench;
+reg [3:0]a,b;
+wire f1, f2, f3;
+
+comp c(a,b,f1,f2,f3);
+
+initial
+begin
+
+$monitor(,$time,"a=%4b b=%4b f1=%b f2=%b f3=%b",a,b,f1,f2,f3);
+
+#0 a = 4'b0000;
+#0 b = 4'b0001;
+
+repeat(8)
+
+#5 a = a+4'b0101;
+#5 b = b+4'b1000;
+end
+
+initial
+begin
+$dumpfile("4bitmagnitude.vcd");
+$dumpvars();
+end
+
+endmodule
