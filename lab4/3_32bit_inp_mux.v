5f8b9096f346660f31767f31dd3a0ac73d968117
Abhilash <abhineog97@gmail.com>
2018-10-23 22:25:37 +0530

    changes

--- lab4/3_32bit_inp_mux.v
+++ lab4/3_32bit_inp_mux.v
@@ -20,7 +20,7 @@ input sel1,sel2;
 genvar j;  //this is the variable that is be used in the generate //block. Don't use integer;   
 generate  for (j=0; j<8;j=j+1) begin: mux_loop  
 //mux_loop is the name of the loop   
-mux2to1 m1(out[j],sel1, sel2,in1[j],in2[j],in3[j],in4[j]);   
+mux4to1 m1(out[j],sel1, sel2,in1[j],in2[j],in3[j],in4[j]);   
 //mux2to1 is instantiated every time it is called . generate can't be written inside an always block
 
 end 
