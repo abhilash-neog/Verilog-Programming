bc710df48e961a75f3676981cb39578ad2c0fd46
Abhilash <abhineog97@gmail.com>
2018-11-07 19:01:39 +0530

    implemented registger

--- Lab5/dff.v
+++ Lab5/dff.v
@@ -1,52 +1,25 @@
-//asynchronous clear; 
-module DFF (Q,QN,D,Clock,ClearN);
- output Q,QN; // data output
- input D; // data input
- input Clock,Clear;
- reg Q;
- always @(posedge Clock, negedge ClearN)
- begin
- if (~Clear)
- Q=1'b0;
- else
- Q=D;
- end
- assign QN = ~Q;
-endmodule
-
 //synchronous clear
-module DFFSync (Q,QN,D,Clock,ClearN);
- output Q,QN; // data output
- input D; // data input
- input Clock,Clear;
- reg Q;
- always @(posedge Clock)
- begin
- if (~Clear)
- Q=1'b0;
- else
- Q=D;
- end
- assign QN = ~Q;
+module d_ff(q,d,clk,reset);
+output [3:0] q; // output data
+input [3:0] d; // input data
+input clk,reset;
+reg [3:0] q;
+always @(posedge clk or negedge reset)
+begin
+if (reset)
+q=d;
+else
+q=4'b0000;
+end
 endmodule
 
-module Register4(Q3,Q2,Q1,Q0,D3,D2,D1,D0,Clock,ClearN);
- output Q3,Q2,Q1,Q0; // output data
- input D3,D2,D1,D0; // input data
- input Clock,ClearN; // clock and clear
- DFF FF0 (Q0,D0,Clock,ClearN);
- DFF FF1 (Q1,D1,Clock,ClearN);
- DFF FF2 (Q2,D2,Clock,ClearN);
- DFF FF3 (Q3,D3,Clock,ClearN);
-endmodule
-
-module reg_32bit(Q,D,Clock,ClearN);
- output [31:0]Q; // output data
- input [31:0]D; // input data
- input Clock,ClearN; // clock and clear
+module reg_32bit(q,d,clk,reset);
+ output [31:0]q; // output data
+ input [31:0]d; // input data
+ input clk,reset; // clock and clear
  genvar j; 
  generate  for (j=0; j<32;j=j+4) begin: mux_loop
- DFF FF0 (Q[j+3:j],D0[j+3:j],Clock,ClearN); 
+ d_ff df(q[j+3:j],d[j+3:j],clk,reset); 
  end
  endgenerate
 endmodule
@@ -58,10 +31,16 @@ wire [31:0] q;
 reg_32bit R(q,d,clk,reset); 
 always @(clk) 
 #5 clk<=~clk; 
+
+initial 
+begin
+$monitor(,$time,"clk=%b out=%b inp=%b reset=%b",clk,d,q,reset);
+end
+
 initial 
 begin 
-clk= 1'b1; 
-reset=1'b0;//reset the register 
+#0  clk = 1'b1; reset = 1'b1; d = 32'hFFFFFFFF;
+#1  reset = 1'b0;
 #20 reset=1'b1; 
 #20 d=32'hAFAFAFAF; 
 #200 $finish; 
