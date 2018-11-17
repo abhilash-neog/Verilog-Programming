2264bd1c6dba6c0b92cb0e75f9b8c54bb3eed391
Abhilash <abhineog97@gmail.com>
2018-11-17 22:41:42 +0530

    lab 8

---
+++ Lab8/alu.v
@@ -0,0 +1,46 @@
+module alu(ctrl,A,B,X,cout);
+input [2:0] ctrl;
+input [3:0] A,B;
+output [3:0] X;
+reg [3:0] X;
+output cout;
+reg cout;
+always@(*)
+begin
+
+if(ctrl==3'b000)
+    {cout,X}=A+B;
+else if(ctrl==3'b001)
+    {cout,X}= A-B;
+else if(ctrl==3'b010)
+    X = A ^ B;
+else if(ctrl==3'b011)
+    X = A | B;
+else if(ctrl==3'b100)
+    X = A & B;
+else if(ctrl==3'b101)
+    X = A ~| B;
+else if(ctrl==3'b110)
+    X = A ~& B;
+else if(ctrl==3'b111)
+    X = A ^~B;
+    
+end
+endmodule
+
+module tb;
+reg [3:0] A,B;
+reg [2:0] ctrl;
+wire [3:0] X;
+wire cout;
+
+alu al(ctrl,A,B,X,cout);
+
+initial begin
+$monitor(,$time,"A=%b B=%b ctrl=%b X=%b",A,B,ctrl,X);
+#0 A = 4'd8; B=4'd1; ctrl=3'b000;
+#5 A = 4'd2; B=4'd3; ctrl=3'b010;
+#10 A = 4'd4; B=4'd5; ctrl=3'b011;
+end
+
+endmodule
---
+++ Lab8/encoder.v
@@ -0,0 +1,33 @@
+module encoder(inp,out);
+input [7:0] inp;
+output [2:0] out;
+reg [2:0] out;
+always@(inp)
+begin
+case(inp)
+    8'b10000000:out=3'b000;
+    8'b01000000:out=3'b001;
+    8'b00100000:out=3'b010;    
+    8'b00010000:out=3'b011;
+    8'b00001000:out=3'b100;
+    8'b00000100:out=3'b101;
+    8'b00000010:out=3'b110;
+    8'b00000001:out=3'b111;
+    default :out=3'bz;
+    endcase 
+end 
+endmodule
+
+module tb;
+reg [7:0] in;
+wire [2:0] out;
+
+encoder enc(in,out);
+
+initial begin
+$monitor(,$time,"inp=%b out=%b",in,out);
+#0 in = 8'd8;
+#5 in = 8'd1;
+end
+
+endmodule
