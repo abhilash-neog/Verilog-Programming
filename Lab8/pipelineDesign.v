c9ed707b565d52f973858f8a79e72f40aa95e599
Abhilash <abhineog97@gmail.com>
2018-11-17 23:23:51 +0530

    lab 8

--- Lab8/pipelineDesign.v
+++ Lab8/pipelineDesign.v
@@ -1,4 +1,13 @@
-module fetch();//stage 1
+`include "encoder.v"
+`include "alu.v"
+`include "parityGenerator.v"
+
+
+module fetch(func,opcode);//stage 1
+input [7:0] func;
+output [2:0] opcode;
+encoder
+
 
 module execute();//stage 2
 
